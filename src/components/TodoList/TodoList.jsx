dc08c3457d16308017246e1c8e1a699ddbbc132e
akashvaidya23 <akash.vaidya23@gmail.com>
2022-03-17 21:28:01 +0530

    completed the project

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,13 +1,39 @@
 import Input from "../Input/Input";
 import Button from "../Button/Button";
 import List from "../List/List";
+import { useState } from "react";
 
 const TodoList = () => {
-    const list = ['l1', 'l2', 'l3', 4];
+    const [list, setList] = useState([]);
+
+    const [inputText, setInputText] = useState('');
+
+    const addTodoListHandler = () => {
+        if (inputText.trim()) {
+            const items = [...list];
+            items.push(inputText);
+            setList(items);
+        }
+        setInputText('');
+    };
+
     return (
         <>
-            <Input />
-            <Button btnText="Add to List" btnClickHandler={() => { console.log('clicked') }} />
+            <Input InputChangeHandler={(e) => {
+                const value = e.target.value;
+                setInputText(value);
+            }}
+                value={inputText}
+                inputKeyChangeHandler={(e) => {
+                    const keycode = e.keyCode;
+                    if (keycode === 13) {
+                        addTodoListHandler();
+                    }
+                }} />
+
+            <Button btnText="Add to List" btnClickHandler={
+                addTodoListHandler
+            } />
             <List list={list} />
         </>
 
